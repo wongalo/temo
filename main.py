0e75c52c24f1171854378888c67a5a52b0773f33
wongalo <58677719+wongalo@users.noreply.github.com>
2026-06-23 17:41:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-print("hello")
-pip install pyinstaller
+
 import tkinter as tk
+root =tk.Tk()
+root.title("builmax")
+root.geometry("600x700")
+
+label=tk.Label(root, text="mara")
+label.pack(pady=20)
+
+button=tk.Button(root, text="close it")
+button.pack()
+
+root.mainloop() 
